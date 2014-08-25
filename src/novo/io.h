20f961ed7a732a5c424ac2c1a5ed37d324e62f71
Xentec <xentec@aix0.eu>
2014-08-25 08:35:04 +0200

    IO: removed some relics

--- src/novo/io.h
+++ src/novo/io.h
@@ -2,9 +2,6 @@
 #define IO_H
 
 #include <novo/global.h>
-
-#include <glm/glm.hpp>
-
 #include <vector>
 
 namespace novo {
@@ -12,7 +9,6 @@ namespace io {
 
 class File {
 public:
-	File(const string& path);
 
 	template<typename Result>
 	static Result get(const string &path);
@@ -20,12 +16,6 @@ public:
 	static string getText(const string &path);
 };
 
-
-string loadFile(const string& file_name);
-
 }}
-
-std::ostream &operator<< (std::ostream &out, const glm::vec3 &vec);
-
 
 #endif // IO_H
