2eeb8dcaed7c226d5c47a72c53d9d4ec4dd42343
xentec <artificial.i7@gmail.com>
2014-09-13 11:34:43 +0200

    GL/Program: improved shader handling

--- src/novo/gfx/gl/shader.h
+++ src/novo/gfx/gl/shader.h
@@ -15,20 +15,18 @@ namespace ShaderType {
 class Shader : public Object<GL_SHADER, glDeleteShader>
 {
 public:
-	Shader(GLenum shader_type, const string& label = "");
 	Shader(GLenum shader_type, const string& glsl_source = "", bool compile_now = true, const string& label = "");
+
 	GLenum getType() const;
 
 	void compile(const string& source = "");
 	bool isCompiled() const;
 
 	static Shader load(GLenum shader_type, const string& path, bool compile_now = true, const string& label = "");
-
 private:
 	GLenum type;
 	string source;
 	bool compiled;
-
 };
 
 }}
