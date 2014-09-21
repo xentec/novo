ee019734c372d64a9c617500fc37de99c349760e
Xentec <xentec@aix0.eu>
2014-09-21 19:26:05 +0200

    IO: corrected template definition (belongs in header) and added FileException

--- src/novo/io.cpp
+++ src/novo/io.cpp
@@ -1,8 +1,5 @@
 #include "io.h"
 
-#include <fstream>
-#include <stdexcept>
-
 namespace novo {
 namespace io {
 
@@ -10,29 +7,10 @@ std::vector<u8> File::getData(const string &path)
 {
 	return get<std::vector<u8>>(path);
 }
+
 string File::getText(const string &path)
 {
 	return get<string>(path);
-}
-
-template<typename Result>
-Result File::get(const string &path)
-{
-	using ifs = std::ifstream;
-	ifs in(path, ifs::binary);
-	if (!in.is_open())
-		throw std::runtime_error(string("Failed to open \"") + path + string("\""));
-
-	// Thanks to <insane coder> for performance tests
-	// http://insanecoding.blogspot.de/2011/11/how-to-read-in-file-in-c.html
-	Result data;
-	in.seekg(0, std::ios::end);
-	data.resize(in.tellg());
-	in.seekg(0, std::ios::beg);
-	in.read(reinterpret_cast<char*>(&data[0]), data.size());
-
-	in.close();
-	return data;
 }
 
 }}
--- src/novo/io.h
+++ src/novo/io.h
@@ -2,19 +2,56 @@
 #define IO_H
 
 #include <novo/global.h>
+#include <novo/exception.h>
+
+#include <fstream>
 #include <vector>
 
 namespace novo {
 namespace io {
+
 
 class File {
 public:
 
 	template<typename Result>
 	static Result get(const string &path);
+
 	static std::vector<u8> getData(const string& path);
 	static string getText(const string &path);
 };
+
+class FileException : public novo::NovoException {
+public:
+	const string path;
+	FileException(string path, string reason):
+		NovoException(string("File ")+reason+string(": ")+path),
+		path(path)
+	{}
+};
+
+// File definitions
+// #################
+template<typename Result>
+Result File::get(const string &path)
+{
+	using ifs = std::ifstream;
+	ifs in(path, ifs::binary);
+	if (!in.is_open())
+		throw FileException(path, "not accessible");
+
+	// Thanks to <insane coder> for performance tests
+	// http://insanecoding.blogspot.de/2011/11/how-to-read-in-file-in-c.html
+	Result data;
+	in.seekg(0, std::ios::end);
+	data.resize(in.tellg());
+	in.seekg(0, std::ios::beg);
+	// TODO: non array version
+	in.read(reinterpret_cast<char*>(&data[0]), data.size());
+
+	in.close();
+	return data;
+}
 
 }}
 
